3f44fcc1e454e612cf8c859a07fe4d2bcb270362
Mahan Mashoof <mahan.mashoof@gmail.com>
2020-05-31 12:28:03 -0300

    cv update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,10 +5,10 @@ import {faDownload} from "@fortawesome/free-solid-svg-icons";
 function Resume() {
   return (<div id="resume" className="row">
     <div className="col">
-      <a href="./../CV_Mahan_Mashoof_Eng.doc" type="button" className="btn btn-dark btn-block" download><FontAwesomeIcon className="icon" icon={faDownload} size="1x"/>Resume (Eng)</a>
+      <a href="./../CV_Mahan_Mashoof_Eng.doc" type="button" className="btn btn-dark btn-block" download><FontAwesomeIcon className="icon" icon={faDownload} size="1x"/>Resume Engineering</a>
     </div>
     <div className="col">
-      <a href="./../Currículo_Mahan_Mashoof_Por.doc" type="button" className="btn btn-dark btn-block" download><FontAwesomeIcon className="icon" icon={faDownload} size="1x"/>Currículo (Por)</a>
+      <a href="./../Resume_Mahan_Mashoof_web.doc" type="button" className="btn btn-dark btn-block" download><FontAwesomeIcon className="icon" icon={faDownload} size="1x"/>Resume Web development</a>
     </div>
   </div>);
 }
